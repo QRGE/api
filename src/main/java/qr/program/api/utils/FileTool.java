e659587155b2144e05cf9b2ff8014c59f12881be
QR <1826255833@qq.com>
2022-05-02 17:23:48 +0800

    简单的FileTool

--- src/main/java/qr/program/api/utils/FileTool.java
+++ src/main/java/qr/program/api/utils/FileTool.java
@@ -1,6 +1,7 @@
 package qr.program.api.utils;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Assert;
 
 import java.io.File;
 
@@ -10,15 +11,32 @@ import java.io.File;
  */
 public class FileTool {
 
+
     public static void main(String[] args) {
-        clearFolderFilePrefix("/Users/qr/Downloads/排序算法");
+        String path = "/Users/qr/Downloads/排序算法";
+        clearFolderFilePrefix(path, "【黑马】数据结构与算法之经典排序算法【配套源码+笔记】 - ");
+    }
+
+    public static void listFolderFiles(String path) {
+        Assert.notBlank(path, "Path must not be blank");
+        File file = FileUtil.file(path);
+        if (file == null || file.isFile()) {
+            System.out.println("Invalid Folder");
+            return;
+        }
+        File[] files = file.listFiles();
+        for (File f : files) {
+            System.out.println(f);
+        }
     }
 
     /**
      * 去除某个文件夹下所有文件的前缀
      * @param path 文件夹路径
      */
-    public static void clearFolderFilePrefix(String path) {
+    public static void clearFolderFilePrefix(String path, String prefix) {
+        Assert.notBlank(path, "Path must not be blank");
+        Assert.notBlank(prefix, "Prefix must not be blank");
         File file = FileUtil.file(path);
         if (file == null || file.isFile()) {
             System.out.println("Invalid Folder");
@@ -29,8 +47,11 @@ public class FileTool {
             System.out.println("Empty Folder");
             return;
         }
-        for (File listFile : files) {
-
+        for (File f : files) {
+            String oldFilename = f.getName();
+            String newFileName = oldFilename.replace(prefix, "");
+            FileUtil.rename(f, newFileName, true);
         }
+        System.out.println("重命名完成");
     }
 }
