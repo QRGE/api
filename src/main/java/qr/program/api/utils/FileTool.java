89da20ed7b6884072a1a66d1121fe1950806107f
QR <1826255833@qq.com>
2022-05-06 04:10:45 +0800

    update FileTool

--- src/main/java/qr/program/api/utils/FileTool.java
+++ src/main/java/qr/program/api/utils/FileTool.java
@@ -13,8 +13,9 @@ public class FileTool {
 
 
     public static void main(String[] args) {
-        String path = "/Users/qr/Downloads/网络编程";
-        clearFolderFilePrefix(path, "从通信协议到Netty RPC框架打造的Java网络编程课，全面又深入，学它就够了 - ");
+        String path = "/Users/qr/Downloads/vue";
+        listFolderFiles(path);
+        clearFolderFilePrefix(path, 2);
     }
 
     public static void listFolderFiles(String path) {
@@ -32,6 +33,32 @@ public class FileTool {
         for (File f : files) {
             System.out.println(f);
         }
+    }
+
+    /**
+     * 清楚文件中文件的前几个字符
+     * @param path 文件夹路径
+     * @param num 前几个字符
+     */
+    public static void clearFolderFilePrefix(String path, Integer num) {
+        Assert.notBlank(path, "Path must not be blank");
+        File file = FileUtil.file(path);
+        if (file == null || file.isFile()) {
+            System.out.println("Invalid Folder");
+            return;
+        }
+        File[] files = file.listFiles();
+        if (files == null || files.length == 0) {
+            System.out.println("Empty Folder");
+            return;
+        }
+        for (File f : files) {
+            String oldFilename = f.getName();
+            String newFileName = oldFilename.substring(num-1);
+            FileUtil.rename(f, newFileName, true);
+            System.out.printf("File: %s rename to %s\n", oldFilename, newFileName);
+        }
+        System.out.println("重命名完成");
     }
 
     /**
